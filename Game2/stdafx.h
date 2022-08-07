7528a3a91537711496611c8b82f199b121b94cef
smartbh <smartbh@naver.com>
2022-08-08 05:32:52 +0900

    file clear

--- Game2/stdafx.h
+++ Game2/stdafx.h
@@ -2,10 +2,6 @@
 #include "../2DFrameWork/framework.h"
 #pragma comment (lib,"../Build/2DFrameWork/2DFrameWork.lib")
 
-#include "ST_Bullet.h"
-#include "ST_playerBullet.h"
-#include "ST_Player.h"
-#include "Enemy.h"
 #include "GameCharacter.h"
 #include "bossFem.h"
 #include "DnFPlayer.h"
